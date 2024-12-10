1de1ac716d3e2e98d46bf996210845906016944a
MrGAFs <gogo172000@gmail.com>
2024-12-10 14:53:40 +0200

    Init Time Capsule

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+app.get("/", (_req: express.Request, res: express.Response) => {
+  res.send(`
+    <h1>Hello this is time Capsule</h1>
+    `);
+
+});
+
+app.listen(port, () => {
+  console.log(`The server is up 📶 => http://localhost:${port} 🚀`);
+});
